ef122cc0219850b7d14fbeed5d84e3dac6ed1fdd
mirei965 <info@pave-up.co.jp>
2025-04-23 03:43:07 +0900

    RPCクライアント関数のポート環境変数を修正し、接続エラーハンドリングを追加しました。また、接続メッセージを更新しました。

--- cmd/cli/rcp.go
+++ cmd/cli/rcp.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/fatih/color"
 	"net/rpc"
 	"os"
-
-	"github.com/fatih/color"
 )
 
-func rpcClient(inMaintenanceMode bool) {
-  rcpPort := os.Getenv("RCP_PORT")
-	c, err := rpc.Dial("tcp", "127.0.0.1:"+rcpPort)
+func rpcClient(inMaintenanceMode bool){
+	rpcPort := os.Getenv("RPC_PORT")
+	c, err := rpc.Dial("tcp", "127.0.0.1:" + rpcPort)
 	if err != nil {
 		exitGracefully(err)
 	}
-	fmt.Println("Connected ...", rcpPort)
 
-  var result string
-  err = c.Call("RPCServer.MaintenanceMode", inMaintenanceMode, &result)
-  if err != nil {
-    exitGracefully(err)
-  }
-  color.Yellow(result)
+	fmt.Println("Connected...")
+	var result string
+	err = c.Call("RPCServer.MaintenanceMode", inMaintenanceMode, &result)
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	color.Yellow(result)
 }
